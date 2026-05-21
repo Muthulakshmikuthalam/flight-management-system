49130a95601ed6e3843c0e2927a290982c062894
Muthulakshmi Kuthalam <muthulakshmikuthalam095@gmail.com>
2026-05-21 21:29:31 +0530

    Added seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,20 @@
+insert into flights
+(flight_no,origin,destination,price)
+
+values
+('AI101','Chennai','Delhi',5500),
+
+('6E220','Mumbai','Bangalore',4300),
+
+('SG450','Hyderabad','Kolkata',6200),
+
+('AI890','Delhi','Goa',7000);
+
+
+insert into bookings
+(pnr_code,seat_no,total_price,status)
+
+values
+('SK1234','A1',5500,'Confirmed'),
+
+('SK4567','B2',4300,'Confirmed');
